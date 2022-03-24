c19762388ddc7f6b54e4443217b95e6015521f6b
llarnold005 <74628922+llarnold005@users.noreply.github.com>
2022-03-24 18:25:55 -0500

    cleaned up code

--- db_tagger.py
+++ db_tagger.py
@@ -2,42 +2,42 @@ from urllib import response
 import boto3
 
 
-
 def tag_dbs(response_iterator):
     databases = []
     for db in response_iterator:
-        db_instances = db['DBInstances']
+        db_instances = db["DBInstances"]
         for db_info in db_instances:
             databases.append(
                 {
-    "db_arn": db_info['DBInstanceArn'],
-    "db_name": db_info['DBInstanceIdentifier'],
-})
-    return(databases)
-   
+                    "db_arn": db_info["DBInstanceArn"],
+                    "db_name": db_info["DBInstanceIdentifier"],
+                }
+            )
+    return databases
+
+
 def add_tags_to_resource(databases, client):
     try:
         for database in databases:
             client.add_tags_to_resource(
-        ResourceName = database['db_arn'],
-        Tags = [
-            {
-                'Key': 'Name',
-                'Value': database['db_name']
-            },
-        ])
-       
+                ResourceName=database["db_arn"],
+                Tags=[
+                    {"Key": "Name", "Value": database["db_name"]},
+                ],
+            )
+
         return True
     except Exception as e:
-        return(e)
-       
-       
+        return e
+
 
 def main():
-    client = boto3.client('rds')
-    paginator = client.get_paginator('describe_db_instances')
-    response_iterator = paginator.paginate()  
+    client = boto3.client("rds")
+    paginator = client.get_paginator("describe_db_instances")
+    response_iterator = paginator.paginate()
     var = tag_dbs(response_iterator)
     results = add_tags_to_resource(var, client)
     print(results)
+
+
 main()
